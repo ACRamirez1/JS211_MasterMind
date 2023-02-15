fdaa64f5d7e6f63f83c784a593a7de9c7f3bae83
ACRamirez1 <caramirez711@gmail.com>
2023-02-15 15:14:08 -0600

    initial commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const readline = require('readline');
+const { string } = require('stylelint/lib/formatters');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -28,13 +29,58 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
+const generateHint = (guess) =>  {
+
   // your code here
+  let solutionArray = solution.split('')
+  let guessArray = guess.split('')
+  let correctLetters = 0                        
+  let correctLetterLocation = 0 // record how many correct 'leter-locations' were guessed
+
+//compare the values at each index with a for loop
+for(let i=0; i < solutionArray.length; i++){
+
+    if(solutionArray[i] === guessArray[i]) {
+      correctLetterLocation++;
+      solutionArray[i] = null;
+    }
+
+    
+}
+
+//check for correct letter  in the wrong location
+for(let i=0; i < solutionArray.length; i++){
+  let targetIndex = solutionArray.indexOf(guessArray[i]);
+  if (targetIndex > -1) {
+    correctLetters++
+    solutionArray[targetIndex] = null; 
+  }
+}
+return correctLetterLocation + '-' + correctLetters;
+
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
+
+// solution = 'abcd'; // Comment this out to generate a random solution
   // your code here
+let hint = generateHint(guess)
+
+  board.push(`${guess} - ${hint}`)
+  //Spec 1 - Detect a correct solution: In mastermind(), if the guess you passed in equals the solution, return 'You guessed it!';
+  if(solution === guess){
+    return 'You guessed it!'
+  } else if(board.length === 10) {
+    return `You ran out of turns! The solution was ${solution}.`
+  } else {
+    return 'Guess again.'
+  }
+
+
+  //Define a variable called hint that collects the returned value of generateHint(guess).
+  
+
+  
 }
 
 
